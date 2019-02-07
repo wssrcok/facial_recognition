9a95c39102ccd69bfe484126613efd548f3b3086
Faye <mengfy0718@gmail.com>
2019-02-06 22:57:24 -0800

    fix not image error

--- algorithm2/unknown_recognition.py
+++ algorithm2/unknown_recognition.py
@@ -7,7 +7,7 @@ from PIL import Image
 def get_encoding(file):
     try:
         img_np = face_recognition.load_image_file(file)
-    except OSError:
+    except IOError:
         print(file + "is not an image file.")
     try:
         encoding = face_recognition.face_encodings(img_np,num_jitters=3)[0]
@@ -24,16 +24,15 @@ def retrive_photos(
     unknown_encoding = get_encoding(input_img)
     
     for img_index in range(len(known_encodings)):
-        distances = face_recognition.face_distance(known_encodings[img_index], unknown_encoding)
-        
-        for distance in distances:
-            if distance < 0.4:
-                # print(os.listdir(known_person_dir)[img_index])
-                path = os.path.join(known_person_dir, os.listdir(known_person_dir)[img_index])
-                im = Image.open(path)
-                im.show()
-               
-        
+        if known_encodings[img_index] is not None: 
+            distances = face_recognition.face_distance(known_encodings[img_index], unknown_encoding)
+            
+            for distance in distances:
+                if distance < 0.4:
+                    # print(os.listdir(known_person_dir)[img_index])
+                    path = os.path.join(known_person_dir, os.listdir(known_person_dir)[img_index])
+                    im = Image.open(path)
+                    im.show()
 
 if __name__ == '__main__':
 

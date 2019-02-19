208f674dfd490c7c3f85d75f1c6cad8cea990965
Andrew Liu <andrewliurm@gmail.com>
2019-02-18 18:03:28 -0800

    user login added

--- FacialRecognition/website/urls.py
+++ FacialRecognition/website/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('upload/', UploadMultipleView.as_view(), name='upload'),
     path('backend/', BackendView.as_view(), name='backend'),
     path('download/', DownloadView.as_view(), name='download'),
+    url(r'^signup/$', views.signup_view, name="signup"),
+    url(r'^login/$', views.login_view, name="login"),
+    url(r'^logout/$', views.logout_view, name="logout"),
     url('download/handler', download_handler),
 ]
